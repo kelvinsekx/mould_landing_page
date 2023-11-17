76c4c208911782a3ac5e54ce3bdbd1c87b0d8db4
kelvinsekx <utdkelvin@gmail.com>
2023-11-17 14:04:14 +0000

    change mould to mound

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "./../components/footer/footer"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Mould Up 🎃',
+  title: 'Mound Up 🎃',
   description: 'Sekx work in progress...',
 }
 
