909eb680951d4249b519e2033f34beefaeef4174
kelvinsekx <utdkelvin@gmail.com>
2023-11-24 13:34:36 +0000

    wip:fit_design2

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -8,8 +8,8 @@ export const WaitListButton: React.FC<
   <Link
     href={`/waitlist`}
     className={twMerge(
-      "text-moundUpWhite font-extrabold bg-moundUpGreen rounded py-3 px-2 md:py-[14px] md:px-4 border-none hover:shadow-[inset_100px_0_0_0_#000000] shadow-[inset_0_0_0_0_#E0EBE8] transition-shadow ease-in-out hover:text-[#F1A91B] text-[92%]",
-      className
+      "text-moundUpWhite font-extrabold bg-moundUpGreen rounded p-4 md:py-[18px] border-none hover:shadow-[inset_100px_0_0_0_#000000] shadow-[inset_0_0_0_0_#E0EBE8] transition-shadow ease-in-out hover:text-[#F1A91B] text-[92%]",
+      className,
     )}
   >
     Join the Waitlist
