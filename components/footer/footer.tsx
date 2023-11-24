5201e6a40d790225ef943f794d95384693910ef4
kelvinsekx <utdkelvin@gmail.com>
2023-11-24 12:09:30 +0000

    css:footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -8,10 +8,10 @@ const data = [
     title: "Company",
     items: ["Contact us"],
   },
-  // {
-  //   title: "Legal",
-  //   items: ["Terms of Service", "Privacy policy"],
-  // },
+  {
+    title: "Legal",
+    items: ["Terms of Service", "Privacy policy"],
+  },
   {
     title: "Links",
     items: ["Home", "About us", "Creators", "Angels"],
@@ -22,64 +22,63 @@ const Footer = () => {
   return (
     <footer className={"bg-moundUpBlack text-moundUpWhite " + styles.footer}>
       <Container>
-        <div className="grid gap-10 lg:grid-cols-[1fr_1fr_1fr] text-base py-10 px-4">
+        <div className="grid grid-cols-[1fr_1fr] justify-center gap-10 lg:grid-cols-[1fr_1fr_1fr_1fr] text-base py-10 px-4">
           {data.map((menu, index) => (
             <Footer.UL key={index} {...menu} />
           ))}
-          <section>
-            <header className="font-extrabold">Community</header>
-            <ul>
-              <li>
-                <a
-                  className="flex gap-1 items-center"
-                  href="https://t.me/+7s2RjGFlLeRiMzY0 "
-                  target="_blank"
-                  rel="noopener noreferrer"
+
+          <ul>
+            <header className="font-extrabold mb-4 text-xl">Community</header>
+            <li>
+              <a
+                className="flex gap-1 items-center"
+                href="https://t.me/+7s2RjGFlLeRiMzY0 "
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  width="1rem"
+                  height="1rem"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <svg
-                    width="1rem"
-                    height="1rem"
-                    viewBox="0 0 25 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M23.374 4.81448L20.0732 20.3809C19.8242 21.4795 19.1748 21.753 18.2519 21.2354L13.2227 17.5293L10.7959 19.8633C10.5273 20.1319 10.3027 20.3565 9.78515 20.3565L10.1465 15.2344L19.4678 6.81155C19.873 6.45022 19.3799 6.25003 18.8379 6.61136L7.31445 13.8672L2.35351 12.3145C1.27441 11.9776 1.25488 11.2354 2.57812 10.7178L21.9824 3.24222C22.8809 2.9053 23.667 3.44241 23.374 4.81448Z"
-                      fill="#FFFDFA"
-                    />
-                  </svg>
-                  <span>Telegram</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://chat.whatsapp.com/JRLJZuByl6cD2RmYGny41R"
-                  className="flex gap-1 items-center"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  <path
+                    d="M23.374 4.81448L20.0732 20.3809C19.8242 21.4795 19.1748 21.753 18.2519 21.2354L13.2227 17.5293L10.7959 19.8633C10.5273 20.1319 10.3027 20.3565 9.78515 20.3565L10.1465 15.2344L19.4678 6.81155C19.873 6.45022 19.3799 6.25003 18.8379 6.61136L7.31445 13.8672L2.35351 12.3145C1.27441 11.9776 1.25488 11.2354 2.57812 10.7178L21.9824 3.24222C22.8809 2.9053 23.667 3.44241 23.374 4.81448Z"
+                    fill="#FFFDFA"
+                  />
+                </svg>
+                <span>Telegram</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://chat.whatsapp.com/JRLJZuByl6cD2RmYGny41R"
+                className="flex gap-1 items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  x="0px"
+                  y="0px"
+                  width="1rem"
+                  height="1rem"
+                  viewBox="0 0 50 50"
                 >
-                  <svg
-                    x="0px"
-                    y="0px"
-                    width="1rem"
-                    height="1rem"
-                    viewBox="0 0 50 50"
-                  >
-                    <path
-                      d="M25,2C12.318,2,2,12.318,2,25c0,3.96,1.023,7.854,2.963,11.29L2.037,46.73c-0.096,0.343-0.003,0.711,0.245,0.966 C2.473,47.893,2.733,48,3,48c0.08,0,0.161-0.01,0.24-0.029l10.896-2.699C17.463,47.058,21.21,48,25,48c12.682,0,23-10.318,23-23 S37.682,2,25,2z M36.57,33.116c-0.492,1.362-2.852,2.605-3.986,2.772c-1.018,0.149-2.306,0.213-3.72-0.231 c-0.857-0.27-1.957-0.628-3.366-1.229c-5.923-2.526-9.791-8.415-10.087-8.804C15.116,25.235,13,22.463,13,19.594 s1.525-4.28,2.067-4.864c0.542-0.584,1.181-0.73,1.575-0.73s0.787,0.005,1.132,0.021c0.363,0.018,0.85-0.137,1.329,1.001 c0.492,1.168,1.673,4.037,1.819,4.33c0.148,0.292,0.246,0.633,0.05,1.022c-0.196,0.389-0.294,0.632-0.59,0.973 s-0.62,0.76-0.886,1.022c-0.296,0.291-0.603,0.606-0.259,1.19c0.344,0.584,1.529,2.493,3.285,4.039 c2.255,1.986,4.158,2.602,4.748,2.894c0.59,0.292,0.935,0.243,1.279-0.146c0.344-0.39,1.476-1.703,1.869-2.286 s0.787-0.487,1.329-0.292c0.542,0.194,3.445,1.604,4.035,1.896c0.59,0.292,0.984,0.438,1.132,0.681 C37.062,30.587,37.062,31.755,36.57,33.116z"
-                      fill="#FFFDFA"
-                    ></path>
-                  </svg>
-                  <span>WhatsApp</span>
-                </a>
-              </li>
-            </ul>
-          </section>
+                  <path
+                    d="M25,2C12.318,2,2,12.318,2,25c0,3.96,1.023,7.854,2.963,11.29L2.037,46.73c-0.096,0.343-0.003,0.711,0.245,0.966 C2.473,47.893,2.733,48,3,48c0.08,0,0.161-0.01,0.24-0.029l10.896-2.699C17.463,47.058,21.21,48,25,48c12.682,0,23-10.318,23-23 S37.682,2,25,2z M36.57,33.116c-0.492,1.362-2.852,2.605-3.986,2.772c-1.018,0.149-2.306,0.213-3.72-0.231 c-0.857-0.27-1.957-0.628-3.366-1.229c-5.923-2.526-9.791-8.415-10.087-8.804C15.116,25.235,13,22.463,13,19.594 s1.525-4.28,2.067-4.864c0.542-0.584,1.181-0.73,1.575-0.73s0.787,0.005,1.132,0.021c0.363,0.018,0.85-0.137,1.329,1.001 c0.492,1.168,1.673,4.037,1.819,4.33c0.148,0.292,0.246,0.633,0.05,1.022c-0.196,0.389-0.294,0.632-0.59,0.973 s-0.62,0.76-0.886,1.022c-0.296,0.291-0.603,0.606-0.259,1.19c0.344,0.584,1.529,2.493,3.285,4.039 c2.255,1.986,4.158,2.602,4.748,2.894c0.59,0.292,0.935,0.243,1.279-0.146c0.344-0.39,1.476-1.703,1.869-2.286 s0.787-0.487,1.329-0.292c0.542,0.194,3.445,1.604,4.035,1.896c0.59,0.292,0.984,0.438,1.132,0.681 C37.062,30.587,37.062,31.755,36.57,33.116z"
+                    fill="#FFFDFA"
+                  ></path>
+                </svg>
+                <span>WhatsApp</span>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="md:text-center py-10">
+        <div className="text-center py-10">
           <InlineIcons />
-          <p className="pt-[18px]">
-            2021 MoundUp Africa inc. All rights reserved
+          <p className="pt-[18px] text-[0.64rem]">
+            2021 MoundUp Africa inc. All rights reserved.
           </p>
         </div>
       </Container>
@@ -89,40 +88,38 @@ const Footer = () => {
 
 const UL = ({ title, items }: { title: string; items: string[] }) => {
   return (
-    <section>
-      <header className="font-extrabold">{title}</header>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {title === "Company" && item === "Contact us" ? (
-              <a href="mailto:info@moundup.africa">{item}</a>
-            ) : (
-              <Link
-                href={(function () {
-                  const i = item.toLowerCase();
-                  if (i === "creators" || i === "angels") {
-                    return "/waitlist";
-                  } else if (i === "about us") {
-                    return "#about";
-                  } else {
-                    return "/";
-                  }
-                })()}
-              >
-                {item}
-              </Link>
-            )}
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul>
+      <header className="font-extrabold mb-4 text-xl">{title}</header>
+      {items.map((item, index) => (
+        <li key={index}>
+          {title === "Company" && item === "Contact us" ? (
+            <a href="mailto:info@moundup.africa">{item}</a>
+          ) : (
+            <Link
+              href={(function () {
+                const i = item.toLowerCase();
+                if (i === "creators" || i === "angels") {
+                  return "/waitlist";
+                } else if (i === "about us") {
+                  return "#about";
+                } else {
+                  return "/";
+                }
+              })()}
+            >
+              {item}
+            </Link>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 };
 
 Footer.UL = UL;
 
 const InlineIcons = () => (
-  <span className="flex md:justify-center gap-2">
+  <span className="flex justify-center gap-2">
     <a
       href="https://web.facebook.com/people/MoundUp-Africa/100075793212356/"
       target="_blank"
