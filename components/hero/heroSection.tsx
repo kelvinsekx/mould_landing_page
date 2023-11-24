f38dd7404c6b6482de8b471526af35ca9588fd15
kelvinsekx <utdkelvin@gmail.com>
2023-11-24 13:26:44 +0000

    wip:fit_design

--- components/hero/heroSection.tsx
+++ components/hero/heroSection.tsx
@@ -4,10 +4,10 @@ import { WaitListButton } from "../button/button";
 export const HeroSection = () => (
   <div className="flex flex-col md:flex-row font-normal bg-moundUpGreen__light max-w-[100rem] mx-auto ">
     <section className="self-center  w-[90%] lg:w-1/2 text-xl py-30 lg:pl-30">
-      <p className="text-base text-center font-semibold leading-[174%] md:font-normal md:text-left md:text-xl md:leading-[164%]">
+      <p className="text-sm md:text-xl text-center font-semibold leading-[174%] md:font-normal md:text-left md:text-xl md:leading-[164%]">
         THE FUTURE OF STARTUPS IN AFRICA IS HERE!!
       </p>
-      <p className="text-moundUpBlack mt-2 text-lg leading-[173.4%] font-black md:text-moundUpGreen md:text-4xl text-center md:text-left">
+      <p className="text-moundUpBlack mt-1 md:mt-2 text-[25px] leading-[173.4%] font-black md:text-moundUpGreen md:text-4xl text-center md:text-left">
         {`A fully integrated approach to startup funding`.toUpperCase()}
       </p>
       <p className="pt-8 md:pt-10 pb-4 tracking-tight md:tracking-normal">
